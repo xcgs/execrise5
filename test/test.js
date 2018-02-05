be4cace482accd8a670f4a5d7f05ddb62f1d06bc
zuoyan <zuoyan_njupt@163.com>
2018-02-05 16:38:11 +0800

    实现继承

--- test/test.js
+++ test/test.js
@@ -50,6 +50,7 @@ describe('实现一个基类，可以继承，可以监听事件', function () {
           return word
         }
       })
+      
       var myclass = new MyClass
       assert.equal(myclass.getVal(), 'hello world')
       assert.equal(MyClass.say('haha'), 'haha')
